65286ca7623dacb1a09e8fd24e022f57885d6c8a
Alejandroramirez07 <danielrammirez1022@hotmail.com>
2025-07-13 17:13:29 -0500

    model upgrading

--- cdk/pipelines/train.py
+++ cdk/pipelines/train.py
@@ -1,18 +1,19 @@
 from sagemaker.workflow.steps import TrainingStep
 from sagemaker.estimator import Estimator
+from sagemaker.workflow.functions import Join
 import sagemaker
 
 
 def get_training_step(role, pipeline_session, processed_data_uri, model_output_uri):
     estimator = Estimator(
-        image_uri="811284229777.dkr.ecr.us-east-1.amazonaws.com/xgboost:1.7-1",
+        image_uri="683313688378.dkr.ecr.us-east-1.amazonaws.com/sagemaker-xgboost:1.5-1",
         role=role,
         instance_count=1,
-        instance_type="ml.t3.medium",
+        instance_type="ml.m5.large",
         output_path=model_output_uri,
         sagemaker_session=pipeline_session,
         hyperparameters={
-            "objective": "reg:squarederror",
+            "objective": "reg:linear",
             "num_round": "100",
             "max_depth": "5",
             "eta": "0.2",
@@ -27,10 +28,9 @@ def get_training_step(role, pipeline_session, processed_data_uri, model_output_u
         estimator=estimator,
         inputs={
             "train": sagemaker.inputs.TrainingInput(
-                s3_data=processed_data_uri,
+                s3_data=Join(on="/", values=[processed_data_uri, "processed", "processed_data.csv"]),
                 content_type="text/csv"
             )
         }
     )
     return step
-
